f38b62879d7da3123e0993a29902310045b1ee79
Evan Greco <e.greco12@gmail.com>
2012-11-11 20:45:34 -0600

    dump produces ~70 mb of data we can do more later

--- get_city_data.py
+++ get_city_data.py
@@ -2,14 +2,15 @@ import requests
 import json
 
 METRO_ARTIST_PREFIX =  'http://ws.audioscrobbler.com//2.0/?method=geo.getmetroartistchart'
-METRO_ARTIST_POSTFIX = '&api_key=0ea35acc68848f320352bfa76c4c6494&format=json'
+METRO_ARTIST_POSTFIX = '&limit=500&api_key=0ea35acc68848f320352bfa76c4c6494&format=json'
 
 
 cities = requests.get('http://ws.audioscrobbler.com/2.0/?method=geo.getmetros&api_key=0ea35acc68848f320352bfa76c4c6494&format=json').json
 
+file = open("artist_dump.json", "w")
 for city in cities['metros']['metro']:
     city_string = METRO_ARTIST_PREFIX + '&country=' + city['country'] + '&metro=' + city['name'] + METRO_ARTIST_POSTFIX
-    print "CITY: " + str(city['name'])
-    top_artists = requests.get(city_string).json
-    for artist in top_artists['topartists']['artist']:
-        print artist['name']
+    top_artists = requests.get(city_string)
+    print city['country']
+    file.write(json.dumps(top_artists.json))
+
