c11d8751cb3737dd261dac22076a982aa71db8c3
Evan Greco <e.greco12@gmail.com>
2012-11-11 20:13:14 -0600

    we have data

--- get_city_data.py
+++ get_city_data.py
@@ -1,8 +1,15 @@
 import requests
+import json
 
-METRO_ARTIST_PREFIX =  '/2.0/?method=geo.getmetroartistchart'
-METRO_ARTIST_POSTFIX = 'api_key=0ea35acc68848f320352bfa76c4c6494&format=json'
+METRO_ARTIST_PREFIX =  'http://ws.audioscrobbler.com//2.0/?method=geo.getmetroartistchart'
+METRO_ARTIST_POSTFIX = '&api_key=0ea35acc68848f320352bfa76c4c6494&format=json'
 
 
-cities = requests.get('http://ws.audioscrobbler.com/2.0/?method=geo.getmetros&api_key=0ea35acc68848f320352bfa76c4c6494&format=json')
-print r.text
+cities = requests.get('http://ws.audioscrobbler.com/2.0/?method=geo.getmetros&api_key=0ea35acc68848f320352bfa76c4c6494&format=json').json
+
+for city in cities['metros']['metro']:
+    city_string = METRO_ARTIST_PREFIX + '&country=' + city['country'] + '&metro=' + city['name'] + METRO_ARTIST_POSTFIX
+    print "CITY: " + str(city['name'])
+    top_artists = requests.get(city_string).json
+    for artist in top_artists['topartists']['artist']:
+        print artist['name']
