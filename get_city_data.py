c3d3a3b0e74b7dd18b7383e311375119201948d1
mxwebsolutions <adame.brandon@gmail.com>
2012-11-12 21:43:42 -0600

    added newline insertion to script. Removing artist_dump.json and moving to Downloads section of github

--- get_city_data.py
+++ get_city_data.py
@@ -12,5 +12,5 @@ for city in cities['metros']['metro']:
     city_string = METRO_ARTIST_PREFIX + '&country=' + city['country'] + '&metro=' + city['name'] + METRO_ARTIST_POSTFIX
     top_artists = requests.get(city_string)
     print city['country']
-    file.write(json.dumps(top_artists.json))
+    file.write(json.dumps(top_artists.json) + '\n')
 
