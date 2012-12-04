c55e5160c3eac188ae38a987c26cd2f830c9cdee
mxwebsolutions <adame.brandon@gmail.com>
2012-12-04 14:05:58 -0600

    last minute changes

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,8 @@ var tweet_template = _.template($('#tweet_template').html());
 var result_template = _.template($('#result_template').html());
 var alert_template = _.template($('#alert_template').html());
 var city_template = _.template($('#city_template').html());
+var city_ranking_template = _.template($('#city_ranking_template').html());
+var similar_artist_ranking_template = _.template($('#similar_artist_ranking_template').html());
 
 $('#bandsearch form').submit(function(ev) {
     var q = $(this).find('input[name=query]').val();
@@ -19,11 +21,29 @@ function ajax_search(q) {
       timeout:15000,
       success: function(data) {
         var result_div = $('#bandsearch .results');
+
         result_div.empty();
         result_div.show();
         result_div.append($(result_template(data)));
-        var city_divs = _.map(data.cities, city_template);
+        var city_divs = _.map(data.cities, city_ranking_template);
         result_div.append(city_divs.join(''));
+
+	var similar_artists = similar_artist_ranking_template(data.cities[0]);
+	result_div.append(similar_artists);
+
+
+//	var city_result_div = $('#bandsearch .results #city_results');
+//	var similar_artist_div = $('#bandsearch .results .similar_artists');
+
+//	city_result_div.empty();
+//	city_result_div.show();
+//	similar_artist_div.empty();
+//	similar_artist_div.show();
+	
+
+//	city_result_div.append(city_divs.join('')); 
+//	similar_artist_div.append(similar_artists);
+	
       },
       error: function(jqXHR,textStatus,errorThrown) {
         var error;
